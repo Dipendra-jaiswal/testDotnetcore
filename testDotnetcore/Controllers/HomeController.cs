cf864aea95067357b8862fc409042bf60d983657
dipendra-jaiswal <dipendra.jaiswal@hotmail.com>
2020-07-10 22:16:07 +0530

    mvc setup

---
+++ testDotnetcore/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace testDotnetcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+            var person = new 
+            {
+                Name = "dddddd",
+                DateTime = DateTime.Now.ToString()
+            };
+            return Json(person);
+
+        //    return JsonResult({ "Id" = 1,"Name" ="ddddd"});
+        }
+    }
+}
